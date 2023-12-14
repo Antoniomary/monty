3af8290009b80d5304e1c95a50b3661f87b11a88
Antoniomary <amanso.totti@yahoo.com>
2023-12-14 08:26:58 -0800

    main.c

--- main.c
+++ main.c
@@ -52,26 +52,6 @@ int main(int ac, char **av)
 }
 
 /**
- * is_valid_number - a function that checks if a string is made up of
- * only numbers.
- * @str: the string to check.
- *
- * Return: 1 if true else 0.
- */
-int is_valid_number(char *str)
-{
-	int i = 0;
-
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-
-	for ( ; str[i] && isdigit(str[i]); ++i)
-		;
-
-	return (str[i] == '\0');
-}
-
-/**
  * readline - a function that reads text from a file descriptor.
  * @line: pointer to hold the retrieved text.
  * @fd: the file descriptor.
