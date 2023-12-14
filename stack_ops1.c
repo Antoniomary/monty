4c96e95edb2c76901c2bfb1f6c6cdef39d52b161
Antoniomary <amanso.totti@yahoo.com>
2023-12-14 01:43:01 -0800

    stack_ops1.c

--- stack_ops1.c
+++ stack_ops1.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * add - a function adds the top two elements of the stack.
+ * add - a function that adds the top two elements of the stack.
  * @head: a double pointer to head node of the stack.
  * @line_number: the line the opcode was passed.
  */
@@ -15,5 +15,35 @@ void add(stack_t **head, unsigned int line_number)
 		print_err(*head, err_msg, line_number, NULL);
 
 	info.top->prev->n += info.top->n;
-	free_second_top();
+	pop(head, line_number);
+}
+
+/**
+ * sub - a function that subtracts the top element from the second
+ * top element of the stack.
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ */
+void sub(stack_t **head, unsigned int line_number)
+{
+	char *err_msg = "can't sub, stack too short";
+
+	(void) head;
+
+	if (!info.top || !info.top->prev)
+		print_err(*head, err_msg, line_number, NULL);
+
+	info.top->prev->n -= info.top->n;
+	pop(head, line_number);
+}
+
+/**
+ * nop - a function that doesn’t do anything.
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void) head;
+	(void) line_number;
 }
