39342fb29e027f3f77780c8d1d022d2c8cbf64a9
Antoniomary <amanso.totti@yahoo.com>
2023-12-14 09:13:05 -0800

    stack_ops2.c

--- stack_ops2.c
+++ stack_ops2.c
@@ -66,3 +66,57 @@ void pstr(stack_t **head, unsigned int line_number)
 
 	putchar('\n');
 }
+
+/**
+ * rotl - a function that rotates the stack to the top.
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ *
+ * Description: The top element of the stack becomes the last one, and the
+ * second top element of the stack becomes the first one.
+ */
+void rotl(stack_t **head, unsigned int line_number)
+{
+	stack_t *temp = info.top;
+
+	(void) line_number;
+
+	if (*head && (*head)->next)
+	{
+		info.top = info.top->prev;
+		info.top->next = NULL;
+
+		temp->next = *head;
+		temp->prev = NULL;
+		(*head)->prev = temp;
+
+		*head = temp;
+	}
+}
+
+/**
+ * rotr - a function that rotates the stack to the bottom.
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ *
+ * Description: The last element of the stack becomes the top element of the
+ * stack.
+ */
+void rotr(stack_t **head, unsigned int line_number)
+{
+	stack_t *temp = *head;
+
+	(void) line_number;
+
+	if (*head && (*head)->next)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+
+		info.top->next = temp;
+		temp->prev = info.top;
+		temp->next = NULL;
+
+		info.top = info.top->next;
+	}
+}
