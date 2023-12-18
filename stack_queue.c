5df7a8933ed083f92618fe0c8679c4e0491d49da
Antoniomary <amanso.totti@yahoo.com>
2023-12-17 16:06:50 -0800

    stack_queue.c

---
+++ stack_queue.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * stack - sets the format fo the data to a stack (LIFO)
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ */
+void stack(stack_t **head, unsigned int line_number)
+{
+	if (info.mode == QUEUE)
+	{
+		info.mode = STACK;
+		info.top = info.front;
+	}
+	else if (info.mode == DEFAULT)
+		info.mode = STACK;
+}
+
+/**
+ * queue - sets the format fo the data to a queue (FIFO)
+ * @head: a double pointer to head node of the stack.
+ * @line_number: the line the opcode was passed.
+ */
+void queue(stack_t **head, unsigned int line_number)
+{
+	if (info.mode == DEFAULT)
+	{
+		info.mode = QUEUE;
+	}
+	else if (info.mode == STACK)
+	{
+		info.mode = QUEUE;
+		info.front = info.top;
+	}
+}
